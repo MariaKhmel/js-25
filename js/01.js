133840436c43acc591bba0d331080433ebbff410
Maria <mariakhmel8@gmail.com>
2025-12-09 01:57:26 +0200

    01

--- js/01.js
+++ js/01.js
@@ -49,8 +49,76 @@
 
 // const x1 = 10;
 // const x2 = 30;
-// const number = 20;
+// const number = 50;
 
 // console.log(`Number ${number} is included in the segment before x1`, number < x1)
 // const res = number > x1 && number < x2;
 // console.log(`Number ${number} is included in the segment`, res);
+// const res = number < x1 || number > x2;
+// console.log(`Is ${number} less than x1 and bigger than x2`, res);
+
+// const isOnline = true;
+// const isFriend = true;
+// const isDnd = false;
+
+// const canOpenChat = isOnline && isFriend;
+// console.log(canOpenChat);
+
+// const sub = 'pro';
+// const canGetAccess = sub === 'pro' || sub === 'vip';
+
+// console.log('Is there access to content?', sub === canGetAccess);
+
+
+
+/*
+ * Напиши скрипт выбора стоимости отеля по кол-ву звёзд.
+ * 1 - 20$, 2 - 30$, 3 - 50$, 4 - 70$, 5 - 120$
+ *
+ * Если в переменной stars что-то кроме чисел 1-5, выведи строку
+ * 'Такого кол-ва звезд нет'
+ */
+
+const stars = 5;
+let price;
+
+// if (stars === 1) {
+//   price = 20;
+// } else if (stars === 2) {
+//   price = 30;
+// } else if (stars === 3) {
+//   price = 50;
+// } else if (stars === 4) {
+//   price = 70;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log("Such amount of starts is not an option.")
+// }
+// console.log(`${stars} hotel price is ${price}$ `);
+
+// switch (stars) {
+//   case 1:
+//     price = 20;
+//     break;
+//   case 2:
+//     price = 30;
+//     break;
+//   case 3:
+//     price = 50;
+//     break;
+//   case 4:
+//     price = 70;
+//     break;
+//   case 5:
+//     price = 120;
+//     break;
+//   default:
+//     console.log(`${stars} starts is not an option.`)
+// }
+
+// console.log(`${stars} star hotel price is ${price}$ `);
+
+
+
+
