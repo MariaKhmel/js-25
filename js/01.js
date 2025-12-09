fb3827cba5b67058bb2ce3dde6d11966d2b6c160
Maria <mariakhmel8@gmail.com>
2025-12-09 02:49:15 +0200

    fx

--- js/01.js
+++ js/01.js
@@ -79,8 +79,8 @@
  * 'Такого кол-ва звезд нет'
  */
 
-const stars = 5;
-let price;
+// const stars = 5;
+// let price;
 
 // if (stars === 1) {
 //   price = 20;
@@ -121,4 +121,74 @@ let price;
 
 
 
+/*
+ * Напиши скрипт выбора стоимости отеля по кол-ву звёзд.
+ * 1,2 - 20$, 3,4 - 30$, 5 - 120$
+ */
 
+// const stars = 1;
+// let price;
+
+// switch (stars) {
+//   case 1:
+//   case 2:
+//     price = 20;
+//     break;
+//   case 3:
+//   case 4:
+//     price = 30;
+//     break;
+//   case 5:
+//     price = 120;
+//     break;
+//   default:
+//     console.log(`${stars} starts is not an option.`)
+// }
+
+// console.log(`${stars} star hotel price is ${price}$ `);
+
+// if (stars === 1 || stars === 2) {
+//   price = 20;
+// } else if (stars === 3 || stars === 4) {
+//   price = 30;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log(`${stars} starts is not an option.`)
+// }
+// console.log(`${stars} star hotel price is ${price}$ `);
+
+/*
+ * Напиши скрипт выбора опции доставки товара.
+ * Опция хранится в переменной option: 1 - самовывоз, 2 - курьер, 3 - почта
+ *
+ * В переменную message записать сообщение в зависимости от опции.
+ * - 'Вы сможете забрать товар завтра с 12:00 в нашем офисе'
+ * - 'Курьер доставит заказ завтра с 9:00 до 18:00'
+ * - 'Посылка будет отправлена сегодня'
+ * - 'Вам перезвонит менеджер'
+ */
+
+// const option = 1;
+// let message;
+
+// switch (option) {
+//   case 1:
+//     message = 'Вы сможете забрать товар завтра с 12:00 в нашем офисе';
+//     break;
+//   case 2:
+//     message = 'Курьер доставит заказ завтра с 9:00 до 18:00';
+//     break;
+//   case 3:
+//     message = 'Посылка будет отправлена сегодня';
+//     break;
+//   default:
+//     message = 'Вам перезвонит менеджер';
+// }
+
+// console.log(message);
+
+for (let i = 0; i < 5; i += 1) {
+  console.log(i)
+}
+console.log('qwe')
