864eef8759af254fd31872ae2651bcb1a66a4db5
Maria <mariakhmel8@gmail.com>
2025-12-10 03:01:24 +0200

    02

--- js/02.js
+++ js/02.js
@@ -1,1 +1,68 @@
-//test
+// const friends = ['a', 'b', 'c', 'd'];
+// const lastIndex = friends.length - 1;
+// console.log(lastIndex);
+// console.log(typeof friends);
+// friends[3] = 'pp';
+// console.table(friends);
+// console.log(friends.length);
+
+// const lastIndex = friends.length - 1;
+// for (let i = 0; i <= lastIndex; i += 1) {
+//   console.log(friends[i]);
+// }
+
+// for (let i = 0; i <= lastIndex; i += 1) {
+//   friends[i] = ` ${friends[i]} - ${i + 1}`;
+
+// }
+
+// console.log(friends);
+
+/*
+ * Посчитать общую сумму покупок в корзине
+ */
+
+const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+
+// let total = 0;
+
+// for (const value of cart) {
+//   total += value;
+// }
+// console.log(total);
+
+// let total2 = 0;
+// for (let i = 0; i <= cart.length - 1; i += 1) {
+//   total2 += cart[i];
+// }
+
+// console.log(total2);
+
+// for (let i = 0; i < cart.length; i += 1) {
+//   cart[i] = Math.round(cart[i] * 1.1);
+// }
+
+// console.log(cart);
+
+/*
+ * Напиши скрипт который подсчитывает сумму всех чётных чисел в массиве.
+ */
+
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+
+// let total = 0;
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] % 2 === 0) {
+//     console.log('even num', numbers[i])
+//     total += numbers[i];
+//   }
+// }
+
+// for (const number of numbers) {
+//   if (number % 2 === 0) {
+//     console.log('even num', number)
+//     total += number;
+//   }
+// }
+// console.log(total);
