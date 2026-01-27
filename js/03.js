ad961649723cac50ce779428ecec45e0bb7701f4
Maria <mariakhmel8@gmail.com>
2026-01-28 00:46:21 +0200

    fx

---
+++ js/03.js
@@ -0,0 +1,50 @@
+/*
+ * Напиши функцию calculateTotalPrice(items)
+ * которая принимает массив цен (чисел) и возвращает их сумму
+ */
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+
+
+// function calculateTotalPrice(items) {
+//   let total = 0;
+//   for (const item of items) {
+//     total += item;
+//   }
+//   return total;
+// }
+
+
+// console.log(calculateTotalPrice(cart));
+
+
+/*
+ * Напиши функцию logItems(items) для перебора и логирования массива
+ */
+
+// const logItems = function (items) {
+//   for (const item of items) {
+//     console.log(item)
+//   }
+// }
+
+// console.log(logItems(['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n']));
+
+
+/*
+ * Напиши функцию findLogin(allLogins, login) для поиска логина
+ * - Если логина нет, вывести сообщение 'Пользователь [логин] не найден.'
+ * - Если нашли логин, вывести сообщение 'Пользователь [логин] найден.'
+ */
+
+const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
+
+const findLogin = function (allLogins, loginToFind) {
+  for (const login of allLogins) {
+    if (login === loginToFind) {
+      return `Login is found ${loginToFind}`;
+    }
+  }
+}
+
+console.log(findLogin(logins, 'k1widab3st'));
