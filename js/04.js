c91995c968ad8a6d2f90187663a4bd4375b015be
Maria <mariakhmel8@gmail.com>
2026-02-24 23:36:36 +0200

    closure

--- js/04.js
+++ js/04.js
@@ -42,26 +42,38 @@
 
 // console.log(filteredArr);
 
-const fruits = [
-  { name: 'apples', quantity: 200, isFresh: true },
-  { name: 'grapes', quantity: 150, isFresh: false },
-  { name: 'bananas', quantity: 100, isFresh: true },
-];
+// const fruits = [
+//   { name: 'apples', quantity: 200, isFresh: true },
+//   { name: 'grapes', quantity: 150, isFresh: false },
+//   { name: 'bananas', quantity: 100, isFresh: true },
+// ];
 
 
-const getFruitWithQuantity = function (fruit) {
-  return fruit.quantity >= 120;
-}
+// const getFruitWithQuantity = function (fruit) {
+//   return fruit.quantity >= 120;
+// }
 
-const filter = function (arr, filterFunc) {
-  const filteredArr = [];
-  for (const el of arr) {
-    const passed = filterFunc(el);
-    if (passed) {
-      filteredArr.push(el);
-    }
+// const filter = function (arr, filterFunc) {
+//   const filteredArr = [];
+//   for (const el of arr) {
+//     const passed = filterFunc(el);
+//     if (passed) {
+//       filteredArr.push(el);
+//     }
+//   }
+//   return filteredArr;
+// }
+// console.log(filter(fruits, getFruitWithQuantity));
+
+const fnA = function (parameter) {
+  const innerVariable = "innerVariable inside fnA"
+  const fnB = function () {
+    console.log(parameter);
+    console.log(innerVariable);
+    console.log("console inside FnB")
   }
-  return filteredArr;
-}
-console.log(filter(fruits, getFruitWithQuantity));
 
+  return fnB;
+}
+const fnC = fnA(555);
+fnC()
