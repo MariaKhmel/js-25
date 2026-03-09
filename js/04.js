ef289ca5597a6257894d64d9ae72d66992958139
Maria <mariakhmel8@gmail.com>
2026-03-09 03:01:42 +0200

    fx

--- js/04.js
+++ js/04.js
@@ -177,3 +177,74 @@
 
 // const test = new User("g");
 // console.log(test.name)
+
+/*
+ * Array.prototype.forEach(callback(currentValue, index, array), thisArg)
+ * - Поэлементо перебирает оригинальный массив
+ * - Ничего не возвращает
+ * - Заменяет классический for, если не нужно прерывать цикл
+ */
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// numbers.forEach(function (el, index, arr) {
+//   console.log(el);
+// }
+// )
+
+/*
+ * Array.prototype.map()
+ * - Поэлементо перебирает оригинальный массив
+ * - Не изменяет оригинальный массив
+ * - Возвращает новый массив такой же длины
+ */
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// function doubledArr(numbers) {
+//   return numbers.map(num => num * 2);
+
+// }
+
+// console.log((doubledArr(numbers)));
+
+const players = [
+  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+  { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+];
+
+// const playersNames = players.map(player => player.name);
+// console.log(playersNames);
+
+// const playerIds = players.map(el => el.id);
+// console.log(playerIds)
+
+/*
+ * Увеличиваем кол-во поинтов каждого игрока на 10%
+ */
+
+// const updatePlayerPoints = players.map(player => ({
+//   ...player,
+//   points: player.points * 1.1,
+// }))
+
+// console.log(players);
+// console.log(updatePlayerPoints);
+
+/*
+ * Увеличиваем кол-во часов игрока по id
+ */
+
+// const playerIdToUpdate = 'player-3';
+
+// const updatePlayerTime = players.map(function (player) {
+//   if (player.id === playerIdToUpdate) {
+//     player.timePlayed += 100;
+//   }
+//   return player;
+// })
+
+// console.log(updatePlayerTime)
