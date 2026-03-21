de4c1d21cdf7a95e04fdfe26ac6c2b6563558429
Maria <mariakhmel8@gmail.com>
2026-03-22 00:18:57 +0200

    fx

--- js/05.js
+++ js/05.js
@@ -1,4 +1,4 @@
-const numbers = [1, 3, 5, 6, 7];
+// const numbers = [1, 3, 5, 6, 7];
 
 // console.log([9, 5, 3, 4, 5].reverse());
 
@@ -11,5 +11,48 @@ const numbers = [1, 3, 5, 6, 7];
 // console.log(sortAsc(numbers));
 
 
-const sortDesc = (numbers) => numbers.sort((a, b) => b - a);
-console.log(sortDesc(numbers))
+// const sortDesc = (numbers) => numbers.sort((a, b) => b - a);
+// console.log(sortDesc(numbers))
+
+// const players = [
+//   { id: 'id-1', name: 'Mango', timePlayed: 120, online: false },
+//   { id: 'id-2', name: 'Poly', timePlayed: 150, online: false },
+//   { id: 'id-3', name: 'Ajax', timePlayed: 60, online: true },
+//   { id: 'id-4', name: 'Chelsey', timePlayed: 770, online: true },
+// ]
+
+// const sortByBestPlayers = function (players) {
+//   return [...players].sort(function (currentEl, nextEl) {
+//     return nextEl.timePlayed - currentEl.timePlayed
+//   })
+// }
+
+
+// function sortByWorstPlayers(players) {
+//   const copyofPlayers = Array.from(players);
+//   return copyofPlayers.sort(function (currentPlayer, nextPlayer) {
+//     return currentPlayer.timePlayed - nextPlayer.timePlayed;
+//   })
+
+// }
+// console.log(sortByWorstPlayers(players))
+// console.log(sortByBestPlayers(players));
+
+// const byName = (players) => players.sort((a, b) => {
+//   const result = a.name[0] > b.name[0];
+//   console.log('result=>', result, a.name[0], b.name[0]);
+
+//   if (result) {
+//     return 1;
+//   }
+
+//   if (!result) {
+//     return - 1;
+//   }
+// })
+
+
+// console.log(byName(players))
+
+// const array = [1, 2, [4, [5]], [6, [7, 8, [9]]]];
+// console.log(array.flat(2));
