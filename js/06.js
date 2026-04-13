c349b76d795d425e5774256fe0aef9c8f7e8eeec
Maria <mariakhmel8@gmail.com>
2026-04-14 01:08:51 +0300

    promise

--- js/06.js
+++ js/06.js
@@ -32,21 +32,32 @@
 // }
 
 
-function promiseAll(promises) {
-  return new Promise((resolve, reject) => {
-    if (promises.length === 0) resolve([]);
+// function promiseAll(promises) {
+//   return new Promise((resolve, reject) => {
+//     if (promises.length === 0) resolve([]);
 
-    const results = new Array(promises.length);
-    let resolved = 0;
+//     const results = new Array(promises.length);
+//     let resolved = 0;
 
-    promises.forEach((promise, index) => {
-      Promise.resolve(promise).then((val) => {
-        results[i] = val;
-        resolved++;
-        if (resolved === promises.length) resolve(results);
-      })
-    }).catch(reject);
-  })
-}
+//     promises.forEach((promise, i) => {
+//       Promise.resolve(promise).then((val) => {
+//         results[i] = val;
+//         resolved++;
+//         if (resolved === promises.length) resolve(results);
+//       }).catch(reject);
+//     })
+//   })
+// }
 
-promiseAll(Promise.resolve(1), Promise.resolve(2), Promise.resolve(3));
+// promiseAll([
+//   Promise.resolve(1),
+//   Promise.resolve(2),
+//   Promise.resolve(3)
+// ]).then(console.log)  // [1, 2, 3]
+
+// promiseAll([Promise.resolve(1), Promise.reject("error"), Promise.resolve(2)]).catch(console.log);
+
+// promiseAll([
+//   new Promise(resolve => setTimeout(() => resolve("slow"), 300)),
+//   new Promise(resolve => setTimeout(() => resolve("fast"), 100))
+// ]).then(console.log)
