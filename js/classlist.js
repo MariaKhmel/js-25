0d117ebf77d4d668582133bcc1cece9a1a01eb72
MariaKhmel <mariakhmel8@gmail.com>
2026-06-02 22:35:46 +0300

    03

---
+++ js/classlist.js
@@ -0,0 +1,7 @@
+const currentPageUrl = "/contact";
+
+const linkEl = document.querySelector(`.site-nav__link[href="${currentPageUrl}"]`);
+
+console.log(linkEl);
+
+linkEl.classList.add("site-nav__link--current");
